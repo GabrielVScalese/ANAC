24cf9bf604ec2c8310a0bd8524b692f482ad0c2f
Gabriel Scalese <cc19171@g.unicamp.br>
2020-06-04 14:17:58 -0300

    Updated methods

--- Anac/src/views/Principal.java
+++ Anac/src/views/Principal.java
@@ -6,6 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import classes.DadosAeroporto;
+import classes.Destino;
+import classes.ListaAeroportos;
+import classes.ListaVoos;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
@@ -23,7 +29,9 @@ public class Principal extends JFrame {
 
 	private JPanel contentPane;
 	private JFrame frame;
-	private JTextField txtCodVoo;
+	private JTextField txtCodAero;
+	private ListaAeroportos listaAeroportos;
+	private JButton btnExcluirVoo;
 
 	/**
 	 * Launch the application.
@@ -70,7 +78,7 @@ public class Principal extends JFrame {
 		contentPane.add(panel_2, BorderLayout.CENTER);
 		panel_2.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Exibir V\u00F4os");
+		JButton btnNewButton = new JButton("Exibir os V\u00F4os");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -89,18 +97,31 @@ public class Principal extends JFrame {
 		panel_2.add(btnNewButton);
 		
 		JButton btnCadastrarAeroporto = new JButton("Cadastrar Aeroporto");
+		btnCadastrarAeroporto.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try
+				{
+					CadastroDeAeroporto frame = new CadastroDeAeroporto();
+					frame.setVisible(true);
+					frame.initialize();
+				}
+				catch (Exception error)
+				{}
+			}
+		});
 		btnCadastrarAeroporto.setFont(new Font("Georgia", Font.PLAIN, 11));
-		btnCadastrarAeroporto.setBounds(161, 126, 179, 23);
+		btnCadastrarAeroporto.setBounds(161, 160, 179, 23);
 		panel_2.add(btnCadastrarAeroporto);
 		
-		JButton btnBuscarVoo = new JButton("Buscar V\u00F4o");
+		JButton btnBuscarVoo = new JButton("Buscar V\u00F4os");
 		btnBuscarVoo.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				try
 				{
 					PesquisaDeVoo frame = new PesquisaDeVoo();
-			        frame.initialize(txtCodVoo.getText());
+			        frame.initialize(txtCodAero.getText());
 					frame.setVisible(true);
 				}
 				catch (Exception error)
@@ -109,28 +130,49 @@ public class Principal extends JFrame {
 		});
 		btnBuscarVoo.setEnabled(false);
 		btnBuscarVoo.setFont(new Font("Georgia", Font.PLAIN, 11));
-		btnBuscarVoo.setBounds(350, 126, 141, 23);
+		btnBuscarVoo.setBounds(161, 126, 179, 23);
 		panel_2.add(btnBuscarVoo);
 		
-		txtCodVoo = new JTextField();
-		txtCodVoo.setFont(new Font("Georgia", Font.PLAIN, 11));
-		txtCodVoo.setHorizontalAlignment(SwingConstants.CENTER);
-		txtCodVoo.addKeyListener(new KeyAdapter() {
+		txtCodAero = new JTextField();
+		txtCodAero.setFont(new Font("Georgia", Font.PLAIN, 11));
+		txtCodAero.setHorizontalAlignment(SwingConstants.CENTER);
+		txtCodAero.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent arg0) {
 				btnBuscarVoo.setEnabled(true);
+				btnExcluirVoo.setEnabled(true);
 			}
 		});
-		txtCodVoo.setBounds(161, 81, 179, 20);
-		panel_2.add(txtCodVoo);
-		txtCodVoo.setColumns(10);
+		txtCodAero.setBounds(161, 81, 179, 20);
+		panel_2.add(txtCodAero);
+		txtCodAero.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("C\u00F3digo do V\u00F4o");
+		JLabel lblNewLabel_2 = new JLabel("C\u00F3digo do Aeroporto");
 		lblNewLabel_2.setFont(new Font("Georgia", Font.PLAIN, 11));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setBounds(161, 56, 179, 14);
 		panel_2.add(lblNewLabel_2);
+		
+		btnExcluirVoo = new JButton("Excluir V\u00F4o");
+		btnExcluirVoo.setEnabled(false);
+		btnExcluirVoo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try
+				{
+					ExclusaoDeVoo frame = new ExclusaoDeVoo();
+					frame.setVisible(true);
+					frame.initialize(txtCodAero.getText());
+				}
+				catch (Exception error)
+				{}
+			}
+		});
+		btnExcluirVoo.setFont(new Font("Georgia", Font.PLAIN, 11));
+		btnExcluirVoo.setBounds(350, 126, 141, 23);
+		panel_2.add(btnExcluirVoo);
 	}
+	
 	
 	/*public boolean isValid ()
 	{
